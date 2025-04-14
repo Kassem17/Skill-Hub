88e8756c4317467db3db06f87cf927cb413cd506
Kassem Haidar <kassemhaidar551@gmail.com>
2025-04-14 15:17:14 +0300

    final push

--- server/server.js
+++ server/server.js
@@ -3,24 +3,23 @@ import connectDB from "./config/database.js";
 import cors from "cors";
 import userRouter from "./routes/userRouter.js";
 import skillRouter from "./routes/skillRouter.js";
+import progressRouter from "./routes/progressRouter.js";
 
 connectDB();
-dotenv.config();
 const app = express();
-app.use(
-  cors({
-    origin: "https://skill-hub-ndkf.vercel.app/", // Your frontend domain
-    credentials: true,
-  })
-);
+
 app.use(express.json());
 
+
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("API is running");
+  res.send("Api is running");
 });
 
-app.use("/api/user", userRouter);
-app.use("/api/skills", skillRouter);
+app.use("/api/user", express.json(), userRouter);
+app.use("/api/skills", express.json(), skillRouter);
+app.use("/api/progress", express.json(), progressRouter);
 
-// Export the app for Vercel
-export default app;
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
