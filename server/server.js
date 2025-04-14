8d7af2d468a3097e8285d25d0e28b2c1dfa707f9
Kassem Haidar <kassemhaidar551@gmail.com>
2025-04-14 14:59:02 +0300

    final

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ import skillRouter from "./routes/skillRouter.js";
 
 connectDB();
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
