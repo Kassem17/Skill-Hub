0d54220a49ddb280c13e33c93272207534bf200d
Kassem Haidar <kassemhaidar551@gmail.com>
2025-04-15 08:43:15 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import connectDB from "./config/database.js";
 import cors from "cors";
 import userRouter from "./routes/userRouter.js";
 import skillRouter from "./routes/skillRouter.js";
-import progressRouter from "./routes/progressRouter.js";
+
 
 connectDB();
 const app = express();
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", express.json(), userRouter);
 app.use("/api/skills", express.json(), skillRouter);
-app.use("/api/progress", express.json(), progressRouter);
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
